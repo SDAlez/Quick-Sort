9d0c9de3c48b2be4fcdcedcf1da86ff02fbfd4eb
Alexandre Carneiro <alecarneiro2007@gmail.com>
2021-04-03 21:21:53 -0300

    Adicionado o método de partição
    adicionado o método de partição onde o pivot é escolhido e a partição do Quick Sort é feito

--- quick-Sort.js
+++ quick-Sort.js
@@ -1,8 +1,28 @@
 const array = [5,3,7,6,2,9];
+partition(array, 0, array.length-1)
+console.log(array)
 
 function swapArray(array, item1, item2) {
     let aux;
     aux = array[item1]
     array[item1] = array[item2]
     array[item2] = aux
+}
+
+function partition(array, left, right) {
+    let pivot = array[Math.floor((left+right)/2)]
+    let itemLeft = left
+    let itemRight = right
+    while(array[itemLeft] < pivot) {
+        itemLeft++
+    }
+    while(array[itemRight] > pivot) {
+        itemRight--
+    }
+    if(itemLeft <= itemRight) {
+        swapArray(array, itemRight, itemLeft)
+        itemLeft++
+        itemRight--
+    }
+    return itemLeft
 }
